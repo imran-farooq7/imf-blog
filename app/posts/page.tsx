76d534ec117345af941ef82660f536845eb1b35a
imran farooq <imran_farooq7@hotmail.com>
2023-07-28 03:46:43 +0500

    posts page  completed

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,32 @@
-type Props = {};
-const PostsPage = (props: Props) => {
-	return <div>PostsPage</div>;
+import Posts from "@/components/Posts";
+import "@/app/globals.css";
+
+const PostsPage = () => {
+	const Dummy_Posts = [
+		{
+			title: "Next js 13",
+			excerpt: "Next js 13 features",
+			date: "2022-01-10",
+			image: "/next.js image component.png",
+		},
+		{
+			title: "Next js 13",
+			excerpt: "Next js 13 features",
+			date: "2022-01-10",
+			image: "/next.js image component.png",
+		},
+		{
+			title: "Next js 13",
+			excerpt: "Next js 13 features",
+			date: "2022-01-10",
+			image: "/next.js image component.png",
+		},
+	];
+	return (
+		<div className="posts">
+			<h1>All Posts</h1>
+			<Posts posts={Dummy_Posts} />
+		</div>
+	);
 };
 export default PostsPage;
